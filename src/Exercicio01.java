742e4aa9d8d68018aeba7a3a9c6aa46a69d6c819
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2026-08-10 17:13:53 -0300

    exercicio 01

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Exercicio01 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int[] x = new int[10];
+        int maior = Integer.MIN_VALUE;
+        int menor = Integer.MAX_VALUE;
+
+        // entrada de dados
+        for (int i = 0; i < x.length; i++) {
+            System.out.print("Digite um numero: ");
+            x[i] = sc.nextInt();
+        }
+
+        // localiza o maior e o menor número armazenado
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] > maior) {
+                maior = x[i];
+            }
+            if (x[i] < menor) {
+                menor = x[i];
+            }
+        }
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+    }
 }
