f644cd21f5579dc4b5fc8ac39dea3f1d1875aaff
Laboratório FIAP <marcos.selmini@uol.com.br>
2026-08-13 20:55:48 -0300

    Exercícios 04 e 05

---
+++ src/Exercicio05.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Exercicio05 {
+    static void main() {
+        Random random = new Random();
+        int[] x = new int[6];
+        int aux, j;
+
+        // geração e impressão dos valores
+        System.out.println("Antes da inversão:");
+        for(int i = 0; i < x.length; i++) {
+            x[i] = random.nextInt(1, 30);
+            System.out.print(x[i] + "  ");
+        }
+
+        // inversão dos valores
+        j = x.length - 1;
+        for(int i = 0; i < x.length / 2; i++, j--) {
+            aux = x[i];
+            x[i] = x[j];
+            x[j] = aux;
+        }
+
+        // impressão após a inversão
+        System.out.println("\nDepois da inversão");
+        for(int i = 0; i < x.length; i++) {
+            System.out.print(x[i] + "  ");
+        }
+    }
+}
